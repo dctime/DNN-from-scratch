5c676c5c05f30660a72e2feac4e84352360a4035
DCtime <daniel.chu916@gmail.com>
2024-09-11 19:06:27 +0800

    Feature: openGL used

--- main.cpp
+++ main.cpp
@@ -12,16 +12,34 @@ int main() {
     return -1;
   }
 
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+
   SDL_Window* window = SDL_CreateWindow("Simple DNN", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1080, 720, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+  SDL_GLContext context = SDL_GL_CreateContext(window);
+
+  gladLoadGLLoader(SDL_GL_GetProcAddress);
 
   bool windowRunning = true;
   while (windowRunning) {
+    glViewport(0, 0, 1080, 720);
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
       if (event.type == SDL_QUIT) {
         windowRunning = false;
       }
     }
+
+    glad_glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glad_glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+    SDL_GL_SwapWindow(window);
     
   }
 
